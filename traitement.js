9dbc0f7bcd22e1f5b6a74891d4c3ac9f57f4ed25
Alexis hebrard <alex.hebrard@gmail.com>
2013-05-17 18:22:59 +0200

    couche d'abstraction

--- traitement.js
+++ traitement.js
@@ -1,15 +1,44 @@
-$scope.filtre = null;
 function getAddressListTorrent() {
   return './list_torrent.json';
 }
 
 function listTorrentCtrl($scope, $http) {
-  $http.get(getAddressListTorrent()).success(function(data) {
-    $scope.downloads = data;
+    $scope.downloads = getDownloads();
     angular.forEach($scope.downloads, function(download) {
       download.bar = (download.size_dl / download.size_tot) * 100
+      if (download.bar == 100) {
+        download.bar_type = 'bar-success';
+        if(download.status == 'send') 
+          download.bar_status == 'progress-striped active';
+      }
+      else if (download.status == 'pause') {
+        download.bar_type = 'bar-warning ';
+        download.bar_status = 'progress-striped'
+      }
+      else if(download.status == 'stop')
+        download.bar_type = 'bar-danger';
+      else
+        download.bar_status = 'progress-striped active';
+
+    });
+
+  function getDownloads() {
+    myTab = new Array();
+    $http.get(getAddressListTorrent()).success(function(data){
+      var i = 0;
+      angular.forEach(data, function(d){
+        myTab[i] = new Object();
+        myTab[i].id_dl = d.id_dl;
+        myTab[i].status = d.status;
+        myTab[i].name = d.name;
+        myTab[i].size_tot = d.size_tot;
+        myTab[i].size_dl = d.size_dl;
+        myTab[i].speed = d.speed;
+        myTab[i].date_ajout = d.date_ajout;
+      })
     })
-  });
+    return myTab;
+  }
 
   $scope.orderId = 'id_dl';
   $scope.custumFilter = function(download) {
