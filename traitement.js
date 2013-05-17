528cf6611706c783915580f7d85e87ddfea66470
root <root@serv-alex.alex-serv>
2013-05-17 21:12:01 +0200

    verssion fonctionnel

--- traitement.js
+++ traitement.js
@@ -3,45 +3,46 @@ function getAddressListTorrent() {
 }
 
 function listTorrentCtrl($scope, $http) {
-  function getDownloads() {
-    myTab = new Array();
-    $http.get(getAddressListTorrent()).success(function(data){
+  $http.get(getAddressListTorrent()).success(function(data){
+      $scope.downloads = data;
+      angular.forEach($scope.downloads, function(download) {
+        download.bar = (download.size_dl / download.size_tot) * 100
+        if (download.bar == 100) {
+        download.bar_type = 'bar-success';
+        if(download.status == 'send') 
+        download.bar_status == 'progress-striped active';
+        }
+        else if (download.status == 'pause') {
+        download.bar_type = 'bar-warning ';
+        download.bar_status = 'progress-striped'
+        }
+        else if(download.status == 'stop')
+        download.bar_type = 'bar-danger';
+        else
+        download.bar_status = 'progress-striped active';
+        });
+      });
+
+  /*  function getDownloads() {
+      myTab = new Array();
+      $http.get(getAddressListTorrent()).success(function(data){
       var i = 0;
       angular.forEach(data, function(d){
-        myTab[i] = new Object();
-        myTab[i].id_dl = d.id_dl;
-        myTab[i].status = d.status;
-        myTab[i].name = d.name;
-        myTab[i].size_tot = d.size_tot;
-        myTab[i].size_dl = d.size_dl;
-        myTab[i].speed = d.speed;
-        myTab[i].date_ajout = d.date_ajout;
-        ++i;
+      myTab[i] = new Object();
+      myTab[i].id_dl = d.id_dl;
+      myTab[i].status = d.status;
+      myTab[i].name = d.name;
+      myTab[i].size_tot = d.size_tot;
+      myTab[i].size_dl = d.size_dl;
+      myTab[i].speed = d.speed;
+      myTab[i].date_ajout = d.date_ajout;
       })
-    })
-    return myTab;
-  }
-
-  $scope.downloads = getDownloads();
-  angular.forEach($scope.downloads, function(download) {
-    download.bar = 30;//(download.size_dl / download.size_tot) * 100
-    if (download.bar == 100) {
-      download.bar_type = 'bar-success';
-      if(download.status == 'send') 
-        download.bar_status == 'progress-striped active';
-    }
-    else if (download.status == 'pause') {
-      download.bar_type = 'bar-warning ';
-      download.bar_status = 'progress-striped'
-    }
-    else if(download.status == 'stop')
-      download.bar_type = 'bar-danger';
-    else
-      download.bar_status = 'progress-striped active';
-  });
+      })
+      return myTab;
+      }*/
 
   $scope.orderId = 'id_dl';
   $scope.custumFilter = function(download) {
-   return download.status == 'pause';
- }
+    return download.status == 'pause';
+  }
 }
