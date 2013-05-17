6ed75bedc99da8eaa7c6bfb3d9156d607646dd27
root <root@serv-alex.alex-serv>
2013-05-18 00:41:43 +0200

    binding

--- traitement.js
+++ traitement.js
@@ -2,26 +2,43 @@ function getAddressListTorrent() {
   return './list_torrent.json';
 }
 
+function abstraction(data) {
+  myTab = new Array();
+  var i = 0;
+  angular.forEach(data, function(d){
+    myTab[i] = new Object();
+    myTab[i].id_dl = d.id;
+    myTab[i].status = d.status;
+    myTab[i].name = d.name;
+    myTab[i].size_tot = d.size_tot;
+    myTab[i].size_dl = d.size_dl;
+    myTab[i].speed = d.speed;
+    myTab[i].date_ajout = d.date_ajout;
+    ++i;
+  });
+  return myTab;
+}
+
 function listTorrentCtrl($scope, $http) {
   $http.get(getAddressListTorrent()).success(function(data){
-      $scope.downloads = data;
-      angular.forEach($scope.downloads, function(download) {
-        download.bar = (download.size_dl / download.size_tot) * 100
-        if (download.bar == 100) {
+    $scope.downloads = abstraction(data);
+    angular.forEach($scope.downloads, function(download) {
+      download.bar = (download.size_dl / download.size_tot) * 100
+      if (download.bar == 100) {
         download.bar_type = 'bar-success';
         if(download.status == 'send') 
-        download.bar_status == 'progress-striped active';
-        }
-        else if (download.status == 'pause') {
+          download.bar_status == 'progress-striped active';
+      }
+      else if (download.status == 'pause') {
         download.bar_type = 'bar-warning ';
         download.bar_status = 'progress-striped'
-        }
-        else if(download.status == 'stop')
+      }
+      else if(download.status == 'stop')
         download.bar_type = 'bar-danger';
-        else
+      else
         download.bar_status = 'progress-striped active';
-        });
-      });
+    });
+  });
 
   /*  function getDownloads() {
       myTab = new Array();
@@ -39,10 +56,10 @@ function listTorrentCtrl($scope, $http) {
       })
       })
       return myTab;
-      }*/
+    }*/
 
-  $scope.orderId = 'id_dl';
-  $scope.custumFilter = function(download) {
-    return download.status == 'pause';
+    $scope.orderId = 'id_dl';
+    $scope.custumFilter = function(download) {
+      return download.status == 'pause';
+    }
   }
-}
