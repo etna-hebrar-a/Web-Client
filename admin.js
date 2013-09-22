47982d562c9f76a2d51a77a1c720ccf0eff13284
Alexis Hebrard <alex.hebrard@gmail.com>
2013-09-22 12:52:25 +0200

    supression des user

--- admin.js
+++ admin.js
@@ -3,6 +3,13 @@ function getAddressAdmin() {
   return './admin.json';
 }
 
+Array.prototype.unset = function(val){
+    var index = this.indexOf(val)
+    if(index > -1){
+        this.splice(index,1)
+    }
+}
+
 function abstraction(data) {
   myTab = new Object();
   myTab.ip = data[0].ip;
@@ -12,14 +19,18 @@ function abstraction(data) {
 
 function adminDeamonCtrl($scope, $http) {
   $http.get(getAddressAdmin()).success(function(data){
-    $scope.donnee = abstraction(data)
+    $scope.data = abstraction(data)
   })
   .error(function(data){
     $scope.ip = 'error';
   });
   $scope.addUser = '';
   $scope.add = function() {
-    $scope.donnee.users.push($scope.addUser);
+    $scope.data.users.push($scope.addUser);
     $scope.addUser = '';
+  };
+  
+  $scope.suppr = function(user) {
+    $scope.data.users.unset(user);
   };
 }
